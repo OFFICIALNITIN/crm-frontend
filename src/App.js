eff58e40ef437543a47b8a54022f88b05e7f6638
NITIN <88224042+OFFICIALNITIN@users.noreply.github.com>
2023-04-12 00:51:38 +0530

    dashboard page added

--- src/App.js
+++ src/App.js
@@ -5,12 +5,13 @@ import './App.css';
 
 import Entry from './pages/Entry/Entry.page';
 import DefaultLayout from './layout/DefaultLayout';
+import Dashboard from './pages/dashboard/Dashboard.page';
 
 function App() {
   return (
     <div className="App">
       <DefaultLayout>
-        //Dashboard
+        <Dashboard />
       </DefaultLayout>
     </div>
   );
